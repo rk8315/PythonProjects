db80293d065c736dbb9a4a0a81dc25246f56551f
rk8315 <rkear24@wgu.edu>
2024-02-27 14:57:53 -0500

    day3 done

--- Beginner_Projects/CodingExercises.py
+++ Beginner_Projects/CodingExercises.py
@@ -8,7 +8,7 @@ def BMI_Calc():
 
     BMI = int(weight / (height ** 2))
 
-    print(f"Your BMI is {BMI}%")
+    print(f"Your BMI is {BMI}")
 
 def Life_In_Weeks():
     age = int(input("Enter your age in years: "))
@@ -22,7 +22,52 @@ def Life_In_Weeks():
 #####################
 #       Day 3       #
 #####################
+def Odd_or_Even():
+    number = int(input("Enter an integer: "))
+    if (number % 2 == 0):
+        print(f"{number} is even!")
+    else:
+        print(f"{number} is odd!")
 
+def BMI_Calc_2dot0():
+    height = float(input("Enter your height in meters: "))
+    weight = int(input("Enter your weight in kilograms: "))
+    BMI = int(weight / (height ** 2))
+    if(BMI <= 18.5):
+        print(f"Your BMI is {BMI}, you are underweight.")
+    elif(BMI > 18.5 and BMI < 25):
+        print(f"Your BMI is {BMI}, you have normal weight.")
+    elif(BMI >= 25 and BMI < 30):
+        print(f"Your BMI is {BMI}, you are slightly overweight.")
+    elif(BMI >= 30 and BMI < 35):
+        print(f"Your BMI is {BMI}, you are obese.")
+    elif(BMI >=35):
+        print(f"Your BMI is {BMI}, you are clinically obese.")
 
+def Auto_Pizza_Order():
+    print("Welcome to Python Pizza Deliveries")
+    size = input("What size pizza do you want? (S, M, L): ").lower()
+    add_pepperoni = input("Do you want pepperoni? (Y or N): ").lower()
+    extra_cheese = input("Do you want extra cheese? (Y or N): ").lower()
+    final_bill = 0
 
+    if(size == "s"):
+        final_bill += 15
+    elif(size == "m"):
+        final_bill += 20
+    elif(size == "l"):
+        final_bill += 25
+    else:
+        print("Invalid size selection.")
+
+    if(add_pepperoni == "y"):
+        if(size == "s"):
+            final_bill += 2
+        else:
+            final_bill += 3
+    
+    if(extra_cheese == "y"):
+        final_bill  +=1
+
+    print(f"Your final bill is: ${final_bill}.")
 
---
+++ Beginner_Projects/Day3_TreasureIsland.py
@@ -0,0 +1,33 @@
+print("\nWelcome to Treasure Island. Your mission is to find the treasure.")
+print("You will be given options during your adventure, can you ")
+print("find the treasure or will you be met with an untimely demise?\n")
+
+game_over = False
+
+while not game_over:
+    cross_road_input = input("You're at a cross road that forks in two directions, where do you want to go? (Left or Right): ").lower()
+    if cross_road_input == "right":
+        print("You fall through a booby trapped path and are trapped forever next to other bold adventurers, Game Over!")
+        game_over = True
+        break
+
+    lake_input = input("You come to a lake. THere is an island in the middle of the lake. Do you want to wait for a boat or swim across? (Wait or Swim): ").lower()
+    if lake_input == "swim":
+        print("You are eaten by sharks, Game Over!")
+        game_over = True
+        break
+
+    house_door_input = input("You arrive at the island unharmed. There is a hourse with 3 doors - red, yellow, and blue. Which color door do you choose to enter? (Red, Yellow, or Blue): ").lower()
+    if house_door_input == "blue":
+        print("You enter a room full of man-eating birds, Game Over!")
+        game_over = True
+        break
+    elif house_door_input == "yellow":
+        print("Ahoy, you have found the treaure!")
+        game_over = True
+        break
+    elif house_door_input == "red":
+        print("You enter a dark room and the door locks behind you. The room is cursed and you are banished to the underworld, Game Over!")
+        game_over = True
+        break
+
